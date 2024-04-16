fe67a9f0851ca06ebe4b74ba97109f47d25d5795
Your Name <you@example.com>
2024-04-16 15:42:31 +0200

    Add volkswagen

--- frontend/indiestream/src/karma.conf.js
+++ frontend/indiestream/src/karma.conf.js
@@ -2,6 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 process.env.CHROME_BIN = require('puppeteer').executablePath();
+require('volkswagen');
 
 module.exports = function (config) {
   config.set({
