b444ec9ce131e088f912ee132e3a23625c93a12a
ValentinFutterer <e11904654@student.tuwien.ac.at>
2024-04-17 10:57:56 +0200

    Remove volkswagen 2

--- frontend/indiestream/src/karma.conf.js
+++ frontend/indiestream/src/karma.conf.js
@@ -2,7 +2,6 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 process.env.CHROME_BIN = require('puppeteer').executablePath();
-require('volkswagen');
 
 module.exports = function (config) {
   config.set({
