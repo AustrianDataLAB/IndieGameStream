afa2b06c63fd19cccc6f0dc925423aaa142645a0
Thomas Riegler <thomas.riegler408@gmail.com>
2024-04-17 12:36:11 +0200

    Extendable pipeline for frontend done
    Pipeline can now build images by just inserting image name and directory with the Dockerfile
    into the YAML file. If a "test" stage is present it runs the test. "test" stage
    should not be the last stage in the Dockerfile. Rather, the last stage should be the production-ready
    image definition.

--- frontend/indiestream/src/karma.conf.js
+++ frontend/indiestream/src/karma.conf.js
@@ -10,19 +10,13 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-verbose-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['verbose'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
