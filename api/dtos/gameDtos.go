f5263d3cece330349c6dd5981a9352bfd9db1be6
Kai <kai@fam-pietruska.de>
2024-04-28 23:31:20 +0200

    GameController

---
+++ api/dtos/gameDtos.go
@@ -0,0 +1,20 @@
+package dtos
+
+import (
+	"api/shared"
+	"github.com/google/uuid"
+)
+
+type GetAllGamesResponseBody struct {
+	ID     uuid.UUID         `json:"id"`
+	Title  string            `json:"title"`
+	Status shared.GameStatus `json:"status"`
+	Url    string            `json:"url"`
+}
+
+type GetGameByIdResponseBody struct {
+	ID     uuid.UUID         `json:"id"`
+	Title  string            `json:"title"`
+	Status shared.GameStatus `json:"status"`
+	Url    string            `json:"url"`
+}
