7d09abe4ab1feb661c1de6ffdbb6f12cf95fe6fc
Thomas Riegler <80053365+rieglerthomas@users.noreply.github.com>
2024-05-03 20:15:54 +0200

    Extendable pipeline for frontend done (#19)
    * Extendable pipeline for frontend done
    * Fixed check if test stage exists
    * Enhanced output for existence of test stage
    * Removing test containers after running
    * Removed build from test stage

---
+++ frontend/indiestream/src/karma.conf.js
@@ -0,0 +1,33 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-verbose-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    reporters: ['verbose'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    singleRun: false
+  });
+};
