2dae22307b8a9f3facf4dba2f5bd12f27bc31247
Kai <kai@fam-pietruska.de>
2024-05-30 22:23:59 +0200

    Access-Control-Allow-Origin *

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -87,7 +87,7 @@ func main() {
 }
 
 func CorsHeader(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
