46b6414815ef5c51975f3758b4f0a23a19ba8a63
ValentinFutterer <e11904654@student.tuwien.ac.at>
2024-06-09 15:46:44 +0200

    Improve frontend layout

--- frontend/indiestream/src/app/app.routes.ts
+++ frontend/indiestream/src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import {GameUploadComponent} from "./components/game-upload/game-upload.component";
+import {GamesOverviewComponent} from "./components/games-overview/games-overview.component";
+import {AccountComponent} from "./components/account/account.component";
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
+import {AuthGuard} from "./guards/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
+  {
+    path: 'dashboard',
+    component: GamesOverviewComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'upload',
+    component: GameUploadComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ frontend/indiestream/src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+}
