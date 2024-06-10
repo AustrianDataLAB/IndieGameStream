9e7bd152fa91c6b7b0c4ad5431327e0b6fc09f46
Kai <kai@fam-pietruska.de>
2024-06-10 15:35:48 +0200

    split create client function

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -80,18 +80,23 @@ func setupDatabase() *sql.DB {
 	return db
 }
 
-func getKubeConfig() armcontainerservice.ManagedClustersClientListClusterUserCredentialsResponse {
+func setupManagedClustersClient() *armcontainerservice.ManagedClustersClient {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
-	ctx := context.Background()
 	clientFactory, err := armcontainerservice.NewClientFactory(
 		os.Getenv("AZURERM_SUBSCRIPTION_ID"), cred, nil)
 	if err != nil {
 		log.Fatalf("failed to create client: %v", err)
 	}
-	res, err := clientFactory.NewManagedClustersClient().ListClusterUserCredentials(ctx,
+	return clientFactory.NewManagedClustersClient()
+}
+
+func getKubeConfig() armcontainerservice.ManagedClustersClientListClusterUserCredentialsResponse {
+	managedClustersClient := setupManagedClustersClient()
+
+	res, err := managedClustersClient.ListClusterUserCredentials(context.Background(),
 		os.Getenv("AZURERM_RESOURCE_GROUP_NAME"),
 		os.Getenv("AZURE_AKS_CLUSTER_NAME"),
 		&armcontainerservice.ManagedClustersClientListClusterUserCredentialsOptions{ServerFqdn: nil, Format: nil})
