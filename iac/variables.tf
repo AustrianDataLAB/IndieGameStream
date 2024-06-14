a1657b53ade328033541f9611db2cffa8f70bde5
Jonas Konrad <jonas.konrad2@gmail.com>
2024-06-14 16:45:11 +0200

    Delete unused terraform files

--- iac/variables.tf
+++ iac/variables.tf
@@ -20,15 +20,3 @@ variable "network" {
 variable "myuser"{
   default = "56ea78b9-6d9f-495b-85ac-7caa86ccc191"
 }
-
-variable "tailscale_auth_key" {
-  type = string
-}
-
-variable "tailscale_client_secret" {
-  type = string
-}
-
-variable "tailscale_client_id" {
-  type = string
-}
