4979b01ff0ef9e1dc52a9dc81abec02caa2bab46
Jonas Konrad <jonas.konrad2@gmail.com>
2024-06-14 18:05:04 +0200

    Add stunner operator

--- iac/kubernetes.tf
+++ iac/kubernetes.tf
@@ -8,6 +8,11 @@ resource "azurerm_kubernetes_cluster" "testCluster" {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_B2ms"
+    upgrade_settings {
+      drain_timeout_in_minutes = 0
+      max_surge = "10%"
+      node_soak_duration_in_minutes = 0
+    }
   }
 
   network_profile {
