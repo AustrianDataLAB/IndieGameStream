8a7ded2f74a0e037bedab096f1825b02038ec7fc
Thomas Riegler <thomas.riegler408@gmail.com>
2024-06-14 19:21:36 +0200

    Removed unnecessary Terraform

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,20 +1,9 @@
-
-
 variable "globals" {
   type = map(any)
 
   default = {
     location               = "West Europe"
   }
-}
-variable "network" {
-    type = map(any)
-
-    # most Austrian University VPNs
-    default = { 
-        allowlist_ips = "128.130.0.0/15,193.171.80.0/21,193.170.16.0/20,193.170.185.0/24,129.27.0.0/16,138.232.0.0/16,141.244.0.0/16,143.50.0.0/16,143.205.0.0/16,140.78.0.0/16,193.186.176.0/22,193.186.172.0/22,149.148.0.0/16,192.82.158.0/24,193.171.96.0/21,193.171.104.0/22"
-    }
-  
 }
 
 variable "myuser"{
