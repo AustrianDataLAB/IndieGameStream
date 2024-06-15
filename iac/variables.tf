9383db131bd8518520fd4533eb0c5e734e86a356
Thomas Riegler <thomas.riegler408@gmail.com>
2024-06-15 23:57:57 +0200

    Removed variable

--- iac/variables.tf
+++ iac/variables.tf
@@ -18,5 +18,5 @@ variable "cluster_name" {
 variable "aks_admin_group_object_ids" {
   description = "aks admin group ids"
   type = list(string)
-  default = [var.myuser, "7ab666bb-6355-4240-aa93-16bfbb9fd5f7"]
+  default = ["56ea78b9-6d9f-495b-85ac-7caa86ccc191", "7ab666bb-6355-4240-aa93-16bfbb9fd5f7"]
 }
