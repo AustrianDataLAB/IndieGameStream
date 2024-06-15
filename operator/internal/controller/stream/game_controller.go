350add069a4899d95e950578b37b43e2fd092fd2
Jonas Konrad <jonas.konrad2@gmail.com>
2024-06-15 15:45:16 +0200

    Fix panic

--- operator/internal/controller/stream/game_controller.go
+++ operator/internal/controller/stream/game_controller.go
@@ -565,6 +565,9 @@ func waitForLoadBalancerIP(ctx context.Context, k8sClient client.Client, namespa
 		if len(svc.Status.LoadBalancer.Ingress) > 0 {
 			ip = svc.Status.LoadBalancer.Ingress[0].IP
 			if ip != "" {
+				return true, nil
+			}
+			if len(svc.Status.LoadBalancer.Ingress) > 1 {
 				ip = svc.Status.LoadBalancer.Ingress[1].IP
 				if ip != "" {
 					return true, nil
