3fe56b73b44cde46771ff01a7c791a3e56190aec
Thomas Riegler <thomas.riegler408@gmail.com>
2024-06-16 22:57:32 +0200

    Test

--- iac/main.tf
+++ iac/main.tf
@@ -11,6 +11,7 @@ variable "tenant_id" {
 provider "azurerm" {
   subscription_id   = var.subscription_id
   tenant_id         = var.tenant_id
+  storage_use_azuread        = true
   features {
     key_vault {
       purge_soft_delete_on_destroy    = true
---
+++ iac/sta.tf
@@ -0,0 +1,41 @@
+resource "azurerm_storage_account" "staindiegamestream" {
+  name                            = "staindiegamestream"
+  resource_group_name             = data.azurerm_resource_group.rgruntime.name
+  location                        = data.azurerm_resource_group.rgruntime.location
+
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  account_kind                    = "StorageV2"
+
+  public_network_access_enabled   = false
+
+  nfsv3_enabled                   = true
+  is_hns_enabled                  = true
+
+  network_rules {
+    default_action = "Deny"
+    ip_rules = ["0.0.0.0/0"]
+    bypass = ["AzureServices"]
+  }
+}
+
+
+resource "azapi_resource" "gamesContainer" {
+  type      = "Microsoft.Storage/storageAccounts/blobServices/containers@2023-01-01"
+  name      = "games"
+  parent_id = "${azurerm_storage_account.staindiegamestream.id}/blobServices/default"
+  body = jsonencode({
+    properties = {
+      denyEncryptionScopeOverride = false
+      enableNfsV3AllSquash = false
+      enableNfsV3RootSquash = false
+      immutableStorageWithVersioning = {
+        enabled = false
+      }
+      publicAccess = "None"
+    }
+  })
+  depends_on = [
+    azurerm_storage_account.staindiegamestream
+  ]
+}
