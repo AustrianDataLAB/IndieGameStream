9b949386a1108edfc334c2043804f9f50a419fd8
Thomas Riegler <thomas.riegler408@gmail.com>
2024-06-16 20:58:01 +0200

    Allow GitHub Actions

--- iac/kubernetes.tf
+++ iac/kubernetes.tf
@@ -50,7 +50,7 @@ resource "azurerm_storage_account" "staindiegamestream" {
 
   network_rules {
     default_action                = "Deny"
-    ip_rules                      = ["0.0.0.0/0" ]
+    ip_rules                      = data.github_ip_ranges.ranges.actions_ipv4
     bypass                        = ["AzureServices"] 
   }
 }
